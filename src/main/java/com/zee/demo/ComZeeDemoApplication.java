7b5acf47006c4d45013072d76a482ee84c6d5ed0
vamandeshmukh <vamandeshmukh@gmail.com>
2022-04-28 11:12:25 +0530

    Initial commit

---
+++ src/main/java/com/zee/demo/ComZeeDemoApplication.java
@@ -0,0 +1,13 @@
+package com.zee.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComZeeDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComZeeDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zee/demo/ComZeeDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zee.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComZeeDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
