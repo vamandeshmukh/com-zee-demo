16288a9ebdb7e7e872ee9a604ad39b3a311622c1
vamandeshmukh <vamandeshmukh@gmail.com>
2022-04-28 11:39:55 +0530

    update

--- src/main/java/com/zee/demo/ComZeeDemoApplication.java
+++ src/main/java/com/zee/demo/ComZeeDemoApplication.java
@@ -1,13 +1,17 @@
 package com.zee.demo;
 
-import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ComZeeDemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ComZeeDemoApplication.class, args);
+
+//		SpringApplication.run(ComZeeDemoApplication.class, args);
+
+		System.out.println("main");
+
 	}
 
 }
