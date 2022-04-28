989d03cb3399b488a019ae8141a51ad1e1000088
vamandeshmukh <vamandeshmukh@gmail.com>
2022-04-28 12:47:53 +0530

    commit

--- src/main/java/com/zee/demo/ComZeeDemoApplication.java
+++ src/main/java/com/zee/demo/ComZeeDemoApplication.java
@@ -1,6 +1,6 @@
 package com.zee.demo;
 
-//import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.zee.demo.model.Employee;
@@ -11,14 +11,14 @@ public class ComZeeDemoApplication {
 
 	public static void main(String[] args) {
 
-//		SpringApplication.run(ComZeeDemoApplication.class, args);
+		SpringApplication.run(ComZeeDemoApplication.class, args);
 
 		System.out.println("Start");
 
 		EmployeeService service = new EmployeeService();
 
 		Employee emp1 = new Employee(101, "Sonu", 50000);
-		service.addEmployee(emp1);
+//		service.addEmployee(emp1);
 
 		System.out.println(service.getAllEmployees().toString());
 
