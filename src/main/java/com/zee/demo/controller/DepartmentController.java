dbcf9334a14e98b913ea99802d929b29929f4094
vamandeshmukh <vamandeshmukh@gmail.com>
2022-04-29 14:33:03 +0530

    rest

--- src/main/java/com/zee/demo/controller/DepartmentController.java
+++ src/main/java/com/zee/demo/controller/DepartmentController.java
@@ -2,23 +2,45 @@ package com.zee.demo.controller;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.zee.demo.model.Department;
 import com.zee.demo.service.DepartmentService;
 
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+
 @RestController
+@RequestMapping("/dep")
 public class DepartmentController {
+
+	private Logger LOG = LoggerFactory.getLogger(this.getClass());
 
 	@Autowired
 	private DepartmentService service;
 
+	@GetMapping("/get-all-deps")
 	public List<Department> getAllDeps() {
+		LOG.info("getAllDeps");
 		return service.getAllDepartments();
 	}
 
-	public Department getDepById(int departmentId) {
+	@GetMapping("/get-dep-by-id/{did}")
+	public Department getDepById(@PathVariable(name = "did") int departmentId) {
+		LOG.info(Integer.toString(departmentId));
 		return service.getDepartmentById(departmentId);
 	}
+
+	@PostMapping("/add-dep")
+	public Department addDep(@RequestBody Department department) {
+		LOG.info(department.toString());
+		return service.addDepartment(department);
+	}
+
 }
--- src/main/java/com/zee/demo/controller/EmployeeController.java
+++ src/main/java/com/zee/demo/controller/EmployeeController.java
@@ -11,12 +11,14 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.zee.demo.model.Employee;
 import com.zee.demo.service.EmployeeService;
 
 @RestController
+@RequestMapping("/emp")
 public class EmployeeController {
 
 	private final Logger LOG = LoggerFactory.getLogger(this.getClass());
@@ -24,42 +26,42 @@ public class EmployeeController {
 	@Autowired
 	EmployeeService service;
 
-//	http://localhost:8888/get-all-emps
+//	http://localhost:8888/emp/get-all-emps
 	@GetMapping("/get-all-emps")
 	public List<Employee> getAllEmps() {
 		LOG.info("getAllEmps");
 		return service.getAllEmployees();
 	}
 
-//	http://localhost:8888/get-emp-by-id/{eid}
+//	http://localhost:8888/emp/get-emp-by-id/{eid}
 	@GetMapping("/get-emp-by-id/{eid}")
 	public Employee getEmpById(@PathVariable(name = "eid") int employeeId) {
 		LOG.info(Integer.toString(employeeId));
 		return service.getEmployeeById(employeeId);
 	}
 
-//	http://localhost:8888/get-emp-by-first-name/{firstName}
+//	http://localhost:8888/emp/get-emp-by-first-name/{firstName}
 	@GetMapping("/get-emp-by-first-name/{firstName}")
 	public Employee getEmpByFirstName(@PathVariable(name = "firstName") String firstName) {
 		LOG.info(firstName);
 		return service.getEmployeeByFirstName(firstName);
 	}
 
-	// http://localhost:8888/add-emp
+	// http://localhost:8888/emp/add-emp
 	@PostMapping("/add-emp")
 	public Employee addEmp(@RequestBody Employee employee) {
 		System.out.println(employee.toString());
 		return service.addEmployee(employee);
 	}
 
-//	http://localhost:8888/update-emp
+//	http://localhost:8888/emp/update-emp
 	@PutMapping("/update-emp")
 	public Employee updateEmp(@RequestBody Employee employee) {
 		LOG.info(employee.toString());
 		return service.updateEmployee(employee);
 	}
 
-//	http://localhost:8888//delete-emp/{eid}
+//	http://localhost:8888/emp/delete-emp/{eid}
 	@DeleteMapping("/delete-emp/{eid}")
 	public Employee deleteEmp(@PathVariable(name = "eid") int employeeId) {
 		LOG.info(Integer.toString(employeeId));
