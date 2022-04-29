b58108a154137fefbf2859f16335e393e76721e4
vamandeshmukh <vamandeshmukh@gmail.com>
2022-04-29 10:52:21 +0530

    rest

--- src/main/java/com/zee/demo/service/EmployeeService.java
+++ src/main/java/com/zee/demo/service/EmployeeService.java
@@ -2,6 +2,8 @@ package com.zee.demo.service;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,36 +22,42 @@ import com.zee.demo.repository.EmployeeRepository;
 @Service
 public class EmployeeService {
 
+	private final Logger LOG = LoggerFactory.getLogger(this.getClass());
+
 	@Autowired
 	private EmployeeRepository repository;
 
 	public List<Employee> getAllEmployees() {
-		System.out.println("getAllEmployees");
+		LOG.info("getAllEmployees");
 		return repository.findAll();
 	}
 
 	public Employee getEmployeeById(int employeeId) {
-		System.out.println("getEmployeeById" + employeeId);
+		LOG.info("getEmployeeById" + employeeId);
 		return repository.findById(employeeId).get();
 	}
 
 	public Employee getEmployeeByFirstName(String firstName) {
+		LOG.info("getEmployeeByFirstName " + firstName);
 		// please write necessary code here and in the repository
 		return null;
 	}
 
 	public Employee addEmployee(Employee employee) {
-		System.out.println("addEmployee");
+		LOG.info("addEmployee " + employee.toString());
 //		repository.find
 		return repository.save(employee);
 	}
 
 	public Employee updateEmployee(Employee employee) {
+		LOG.info("updateEmployee " + employee.toString());
+
 		// write logic here
 		return repository.save(employee);
 	}
 
 	public Employee deleteEmployee(int employeeId) {
+		LOG.info("deleteEmployee " + employeeId);
 		Employee emp = repository.findById(employeeId).get();
 		repository.delete(emp);
 		return emp;
